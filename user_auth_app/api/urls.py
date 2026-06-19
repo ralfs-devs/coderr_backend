7c65ad52f81e1f8f259be77f269840921477fd6b
Ralf Hamacher <ralf.hamacher@googlemail.com>
2026-06-19 10:33:08 +0200

    Typo in urls.py corrected

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -12,6 +12,6 @@ from django.urls import path
 from .views import RegistrationView, LoginView
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name='user_registration'),
+    path('registration/', RegistrationView.as_view(), name='user_registration'),
     path('login/', LoginView.as_view(), name='user_login'),
 ]
